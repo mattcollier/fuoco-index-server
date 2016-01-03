0a8cc040e7c0f64c847b2ace2cac57a69b3b4e9b
Matthew Collier <collier.matthew@gmail.com>
2016-01-03 10:40:12 -0500

    Add logging config.

--- configs/live.config.js
+++ configs/live.config.js
@@ -1,6 +1,29 @@
 var config = require('bedrock').config;
 var path = require('path');
 
+// location of logs
+var _logdir = path.join('/var', 'log', 'fuoco-index-server');
+
+// core configuration
+config.core.workers = 1;
+config.core.worker.restart = true;
+
+// master process while starting
+// config.core.starting.groupId = 'adm';
+// config.core.starting.userId = 'root';
+
+// master and workers after starting
+config.core.running.groupId = 'fuoco';
+config.core.running.userId = 'fuoco';
+
+// logging
+config.loggers.app.filename = path.join(_logdir, 'app.log');
+config.loggers.access.filename = path.join(_logdir, 'access.log');
+config.loggers.error.filename = path.join(_logdir, 'error.log');
+config.loggers.email.silent = true;
+config.loggers.email.to = ['cluster@fuoco.floydcommons.com'];
+config.loggers.email.from = 'cluster@fuoco.floydcommons.com';
+
 // server info
 config.server.port = 3100;
 config.server.httpPort = 22080;
