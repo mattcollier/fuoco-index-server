ee1291264c2fd778f54e4d3fd999b33e46588f05
Matthew Collier <collier.matthew@gmail.com>
2016-01-03 10:05:54 -0500

    Update live config to fuoco.floydcommons.com.

--- configs/live.config.js
+++ configs/live.config.js
@@ -4,9 +4,9 @@ var path = require('path');
 // server info
 config.server.port = 3100;
 config.server.httpPort = 22080;
-config.server.bindAddr = ['sterns.t4k.org'];
-config.server.domain = 'sterns.t4k.org';
-config.server.host = 'sterns.t4k.org:' + config.server.port;
+config.server.bindAddr = ['fuoco.floydcommons.com'];
+config.server.domain = 'fuoco.floydcommons.com';
+config.server.host = 'fuoco.floydcommons.com:' + config.server.port;
 config.server.baseUri = 'https://' + config.server.host;
 config.server.key = path.join(__dirname, '..', 'pki', 'privkey.pem');
 config.server.cert = path.join(__dirname, '..', 'pki', 'fullchain.pem');
