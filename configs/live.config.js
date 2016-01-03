b62ee9dcfdb1c45de7ce01049c0d09b77714e835
Matthew Collier <collier.matthew@gmail.com>
2016-01-02 21:28:27 -0500

    Fix-up live config.

--- configs/live.config.js
+++ configs/live.config.js
@@ -8,5 +8,5 @@ config.server.bindAddr = ['sterns.t4k.org'];
 config.server.domain = 'sterns.t4k.org';
 config.server.host = 'sterns.t4k.org:' + config.server.port;
 config.server.baseUri = 'https://' + config.server.host;
-config.server.key = path.join('..', 'pki', 'privkey.pem');
-config.server.cert = path.join('..', 'pki', 'fullchain.pem');
+config.server.key = path.join(__dirname, '..', 'pki', 'privkey.pem');
+config.server.cert = path.join(__dirname, '..', 'pki', 'fullchain.pem');
